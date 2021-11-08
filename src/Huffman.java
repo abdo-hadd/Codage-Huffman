c6a26efbef6f8198dc5e626cc71d8afc56e8655b
abdo-hadd <abelhakimhaddani99@gmail.com>
2021-11-08 23:34:52 +0100

    class arbre

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,5 +1,15 @@
 
 public class Huffman {
+	
+	static abstract class Arbre {
+		void affiche() {
+		    afficheCode("");
+		}
+		abstract void afficheCode(String s);
+		abstract void CompleterRacine(Arbre r);
+		abstract String Decoder(String m,int i);
+	    }
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
